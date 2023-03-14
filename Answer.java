5140108a25a8ab3a4009f281a0b3039202507f11
Subhrajitmishra7 <103630905+Subhrajitmishra7@users.noreply.github.com>
2023-03-14 16:34:54 +0530

    Add files via upload
    It contains codes implementing Inheritance in Java

---
+++ Answer.java
@@ -0,0 +1,45 @@
+class Fruit 
+{  
+String name, taste, size;  
+void eat() 
+{  
+    System.out.println("I am eating fruits today");  
+}  
+}  
+class Apple extends Fruit 
+{  
+Apple() {  
+name = "Apple";  
+taste = "sweet and juicy"; 
+size = "4";
+}  
+
+void eat() {  
+    System.out.println(name + " is "+ taste + " in taste" + "and has a size of " + size);  
+}  
+}  
+class Orange extends Fruit 
+
+{  
+Orange() {  
+    name = "Orange";  
+    taste = "sour,juicy and tangy";
+    size = "5";  
+}    
+void eat()
+{
+System.out.println(name + " is "+ taste + " in taste." + "and has a size of " + size);  
+}  
+}
+public class Answer 
+{  
+    public static void main(String[] args) {  
+                Fruit fruit = new Fruit();  
+                Apple apple = new Apple();  
+                Orange orange = new Orange();  
+                fruit.eat();  
+                apple.eat();  
+                orange.eat();  
+        }  
+    }  
+    
